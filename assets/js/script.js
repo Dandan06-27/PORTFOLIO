c138ecbd7483a3e1c2d742a2182d0dd034671d99
Dandan06-27 <dandantampus02@gmail.com>
2026-03-04 10:51:03 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,13 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+document.onkeydown = function(e) {
+    if (event.keyCode == 123) { // F12 key code
+        return false;
+    }
+    // You can also add other shortcuts like Ctrl+Shift+I (keyCode 73)
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+        return false;
+    }
+}
